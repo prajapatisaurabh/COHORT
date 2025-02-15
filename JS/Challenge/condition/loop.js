cbd7f08906c430af82757b3290172d90cc25a2b2
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2025-02-15 20:08:07 +0530

    loop challenge

---
+++ JS/Challenge/condition/loop.js
@@ -0,0 +1,14 @@
+const links = {
+  about: "About",
+  policy: "Privacy Policy",
+  contact: "Contact Us",
+};
+
+Object.entries(links).forEach((key, value) => {
+  console.log(key);
+  console.log(value);
+});
+
+// links.map((item) => {
+//   console.log(item);
+// });
