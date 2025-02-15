78b666ed4f7ba1c645a099c90dad290167587db4
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2025-02-15 16:04:04 +0530

    JS Challenge 5

---
+++ JS/Challenge/condition/grade.js
@@ -0,0 +1,14 @@
+// You just need to implement the calculateGrade function
+function calculateGrade(marks) {
+  if (marks >= 90) {
+    return "A";
+  } else if (marks > 80 && marks < 89) {
+    return "B";
+  } else if (marks > 70 && marks < 79) {
+    return "C";
+  } else if (marks > 60 && marks < 69) {
+    return "D";
+  } else {
+    return "E";
+  }
+}
