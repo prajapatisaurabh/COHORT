7734ade16a2bf13461d362a0d345ca7f4f941050
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2025-02-16 09:35:36 +0530

    implement the checkNumberType function

---
+++ JS/Challenge/condition/checkNumberType.js
@@ -0,0 +1,10 @@
+// You just need to implement the checkNumberType function
+function checkNumberType(num) {
+  if (num > 0) {
+    return "Positive";
+  } else if (num < 0) {
+    return "Negative";
+  } else {
+    return "Zero";
+  }
+}
