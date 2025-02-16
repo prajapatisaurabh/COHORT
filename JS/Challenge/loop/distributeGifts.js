25a359f908675158b76680c75ca6fefa52087e03
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2025-02-16 09:57:55 +0530

    implement the distributeGifts function add if clause

--- JS/Challenge/loop/distributeGifts.js
+++ JS/Challenge/loop/distributeGifts.js
@@ -1,7 +1,12 @@
 // You just need to implement the distributeGifts function
 function distributeGifts(totalGifts, friends) {
+  if (totalGifts < friends) {
+    return totalGifts;
+  }
   for (let i = 0; i < friends; i++) {
     totalGifts--;
   }
   return totalGifts;
 }
+
+distributeGifts(2, 4);
