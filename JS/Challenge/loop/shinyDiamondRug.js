9a61c461fc40f66d6586ca10cda44aecc5c7d444
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2025-02-16 11:09:53 +0530

    Add return statement in lopop

--- JS/Challenge/loop/shinyDiamondRug.js
+++ JS/Challenge/loop/shinyDiamondRug.js
@@ -1,11 +1,13 @@
 function shinyDiamondRug(n) {
+  let result = "";
   for (let i = 0; i < n; i++) {
-    console.log(" ".repeat(n - i - 1) + "*".repeat(2 * i + 1));
+    result += " ".repeat(n - i - 1) + "*".repeat(2 * i + 1) + "\n";
   }
   // Lower part
   for (let i = n - 2; i >= 0; i--) {
-    console.log(" ".repeat(n - i - 1) + "*".repeat(2 * i + 1));
+    result += " ".repeat(n - i - 1) + "*".repeat(2 * i + 1) + "\n";
   }
+  return result.trim();
 }
 
 function invertedMountain(n) {
