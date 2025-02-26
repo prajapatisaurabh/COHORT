2869afb779f05007e9bd43aa9d96563400c7a85e
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2025-02-26 21:30:25 +0530

    Cohort Week8

---
+++ JS/Week08-26-Feb/datatype and objects.js
@@ -0,0 +1,17 @@
+let num = 10;
+let name = "Name";
+
+let person = {
+  name: "Hitesh",
+  age: 19,
+  isStudent: true,
+};
+
+let num1 = "20";
+let convertedNums = Number(num1);
+console.log(convertedNums);
+console.log(typeof convertedNums);
+
+let convertedNum1 = +num;
+console.log(convertedNum1);
+console.log(typeof convertedNum1);
