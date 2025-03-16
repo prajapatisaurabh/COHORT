b4c84ad1c4e846697f60c6ebf1f3bbd72c15dc16
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2025-03-16 08:06:04 +0530

    Good

--- Projects/FULLSTACK-SQL/controllers/auth.controller.js
+++ Projects/FULLSTACK-SQL/controllers/auth.controller.js
@@ -1,3 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 export const registerUser = async (req, res) => {
   console.log("User registerd");
+  await prisma.user.findUnique({
+    where: { email },
+  });
 };
