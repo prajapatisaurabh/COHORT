ed67790dcec4cf81eb9229302443790c77debe70
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2025-03-22 08:57:08 +0530

    Fix Auth application

--- Projects/FULLSTACK-SQL/controllers/auth.controller.js
+++ Projects/FULLSTACK-SQL/controllers/auth.controller.js
@@ -2,8 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export const registerUser = async (req, res) => {
-  console.log("User registerd");
-  await prisma.user.findUnique({
-    where: { email },
-  });
+  try {
+    const { name, email, password } = req.body;
+
+    
+
+  } catch (error) {}
 };
