5a49400fc215116f0675e806d3a9bb076e0a47c7
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2025-04-05 21:23:46 +0530

    Understand Mail sender and file uploader

---
+++ Projects/MEGA-PROJECT/src/middlewares/multer.middleware.js
@@ -0,0 +1,32 @@
+import multer, { diskStorage } from "multer";
+import { join, extname } from "path";
+
+// Set up storage configuration
+const storage = diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, join(__dirname, "../uploads")); // Specify upload directory
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, `${file.fieldname}-${uniqueSuffix}${extname(file.originalname)}`);
+  },
+});
+
+// File filter to validate file types
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type. Only JPEG, PNG, and GIF are allowed."));
+  }
+};
+
+// Initialize multer
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 }, // Limit file size to 5MB
+  fileFilter: fileFilter,
+});
+
+export default upload;
---
+++ Projects/MEGA-PROJECT/src/utilis/mail.js
@@ -0,0 +1,87 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendMail = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "ThitaInfo inc",
+      link: "https://thitainfo.com",
+    },
+  });
+
+  var emailText = mailGenerator.generatePlaintext(options.mailGenContent);
+
+  var emailHtmlText = mailGenerator.generate(options.mailGenContent);
+
+  // send mail
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_HOST,
+    port: process.env.MAILTRAP_PORT,
+    secure: false, // true for port 465, false for other ports
+    auth: {
+      user: process.env.MAILTRAP_USERNAME,
+      pass: process.env.MAILTRAP_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.MAILTRAP_SENDEREMAIL,
+    to: options.email,
+    subject: options.subject,
+    text: emailText,
+    html: emailHtmlText,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const emailVerificationMailGenContent = (username, verificationURL) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to app thitainfo, welcome to tha team",
+      action: {
+        instruction: "To get Start with aur app",
+        button: {
+          color: "#22BB66",
+          text: "Verify your email",
+          link: verificationURL,
+        },
+      },
+      outro:
+        "Thank you visiting thitainfo, Please share your thougth in linkedin",
+    },
+  };
+};
+
+const forgotPasswordMailGetContent = (username, passwordResetURL) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to app thitainfo, welcome to tha team",
+      action: {
+        instruction: "To get Start with aur app ThitaInfo",
+        button: {
+          color: "#22BB66",
+          text: "Change your password | Reset Password",
+          link: passwordResetURL,
+        },
+      },
+      outro:
+        "Thank you visiting thitainfo, Please share your thougth in linkedin",
+    },
+  };
+};
+
+// sendMail({
+//   email: user.email,
+//   subject: "Interview schedule at 10",
+//   mailGenContent: emailVerificationMailGenContent(
+//     user.username,
+//     username.verificationURL,
+//   ),
+// });
